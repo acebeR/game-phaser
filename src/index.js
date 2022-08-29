294a18445e51fa718d5f12034ddca2997df74cef
Rebeca Divina Paula Carvalhedo <rebecadivinapc@gmail.com>
2022-08-29 01:48:23 -0300

    fases e score

--- src/index.js
+++ src/index.js
@@ -21,11 +21,13 @@ var config = {
 var game = new Phaser.Game(config);
 var score = 0;
 var scoreText;
-
+var plano = "terra";
 
 function preload ()
 {
-    this.load.image('sky', 'src/assets/sky.png');
+    this.load.image('terra', 'src/assets/terra.jpg');
+    this.load.image('sky', 'src/assets/sky.jpg');
+    this.load.image('espaco', 'src/assets/espaco.jpg');
     this.load.image('ground', 'src/assets/platform.png');
     this.load.image('star', 'src/assets/star.png');
     // this.load.image('bomb', 'assets/bomb.png');
@@ -34,11 +36,7 @@ function preload ()
 
 function create ()
 {
-    this.add.image(400, 300, 'sky');
-    this.add.image(100, 568, 'ground');
-    this.add.image(600, 400, 'ground');
-    this.add.image(50, 250, 'ground');
-    this.add.image(750, 220, 'ground');
+    this.add.image(400, 300, 'terra');
 
     this.platforms = this.physics.add.staticGroup();
     this.platforms.create(400, 568, 'ground').setScale(2).refreshBody();
@@ -95,6 +93,62 @@ function collectStar (player, star)
     star.disableBody(true, true);
     score += 10;
     scoreText.setText('Score: ' + score);
+    console.log(plano);
+    if(this.player.y === 180 && plano != 'espaco'){
+        if(plano === "terra"){
+            plano = "sky";
+            this.add.image(400, 300, 'sky');
+        }else if(plano === "sky"){
+            plano = "espaco";
+            this.add.image(400, 300, 'espaco');
+        }
+
+        this.platforms = this.physics.add.staticGroup();
+        this.platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+        this.platforms.create(600, 400, 'ground');
+        this.platforms.create(50, 250, 'ground');
+        this.platforms.create(750, 220, 'ground');
+
+        this.player = this.physics.add.sprite(100, 450, 'dude');
+        this.player.setBounce(0.2);
+        this.player.setCollideWorldBounds(true);
+        this.physics.add.collider(this.player, this.platforms);
+
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'turn',
+            frames: [ { key: 'dude', frame: 4 } ],
+            frameRate: 20
+        });
+
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.stars = this.physics.add.group({
+            key: 'star',
+            repeat: 11,
+            setXY: { x: 12, y: 0, stepX: 70 }
+        });
+        
+        this.stars.children.iterate(function (child) {
+        
+            child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+        
+        });
+        scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+        this.physics.add.collider(this.stars, this.platforms);
+        this.physics.add.overlap(this.player, this.stars,collectStar, null, this);
+    }
 }
 
 function update ()
@@ -124,4 +178,5 @@ function update ()
     {
         this.player.setVelocityY(-330);
     }
+
 }
